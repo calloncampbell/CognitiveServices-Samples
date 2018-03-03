2801a70ee4fc268be0e41666816e8ce392125be3
Callon Campbell <calloncampbell@msn.com>
2018-03-03 15:47:25 -0500

    Add project files.

---
+++ VerifyCrew/MainWindow.xaml.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using Microsoft.ProjectOxford.Face;
+using Microsoft.ProjectOxford.Face.Contract;
+
+namespace VerifyCrew
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly IFaceServiceClient faceServiceClient = new FaceServiceClient("496b22058ec24307bbd09b0654f5fa03", "https://westus.api.cognitive.microsoft.com/face/v1.0");
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+        private async void BrowseButton_Click(object sender, RoutedEventArgs e)
+        {
+            var openDlg = new Microsoft.Win32.OpenFileDialog();
+
+            openDlg.Filter = "JPEG Image(*.jpg)|*.jpg";
+            bool? result = openDlg.ShowDialog(this);
+
+            if (!(bool)result)
+            {
+                return;
+            }
+
+            string filePath = openDlg.FileName;
+
+            Uri fileUri = new Uri(filePath);
+            BitmapImage bitmapSource = new BitmapImage();
+
+            bitmapSource.BeginInit();
+            bitmapSource.CacheOption = BitmapCacheOption.None;
+            bitmapSource.UriSource = fileUri;
+            bitmapSource.EndInit();
+
+            FacePhoto.Source = bitmapSource;
+
+
+            Title = "Detecting...";
+            FaceRectangle[] faceRects = await UploadAndDetectFaces(filePath);
+            Title = String.Format("Detection Finished. {0} face(s) detected", faceRects.Length);
+
+            if (faceRects.Length > 0)
+            {
+                DrawingVisual visual = new DrawingVisual();
+                DrawingContext drawingContext = visual.RenderOpen();
+
+                drawingContext.DrawImage(bitmapSource, new Rect(0, 0, bitmapSource.Width, bitmapSource.Height));
+
+                double dpi = bitmapSource.DpiX;
+                double resizeFactor = 96 / dpi;
+
+                foreach (var faceRect in faceRects)
+                {
+                    drawingContext.DrawRectangle(
+                        Brushes.Transparent,
+                        new Pen(Brushes.Red, 2),
+                        new Rect(
+                            faceRect.Left * resizeFactor,
+                            faceRect.Top * resizeFactor,
+                            faceRect.Width * resizeFactor,
+                            faceRect.Height * resizeFactor
+                            )
+                    );
+                }
+
+                drawingContext.Close();
+
+                RenderTargetBitmap faceWithRectBitmap = new RenderTargetBitmap(
+                    (int)(bitmapSource.PixelWidth * resizeFactor),
+                    (int)(bitmapSource.PixelHeight * resizeFactor),
+                    96,
+                    96,
+                    PixelFormats.Pbgra32);
+
+                faceWithRectBitmap.Render(visual);
+                FacePhoto.Source = faceWithRectBitmap;
+            }
+        }
+
+        private async Task<FaceRectangle[]> UploadAndDetectFaces(string imageFilePath)
+        {
+            try
+            {
+                using (Stream imageFileStream = File.OpenRead(imageFilePath))
+                {
+                    //var faces = await faceServiceClient.DetectAsync(imageFileStream, true, true, );
+
+                    /* NOTES
+                        - Every call to the Face API requires a subscription key. This key needs to be either passed through a query string parameter, or specified in the request header.
+                        - The maximum size of the image is 4 MB.
+                    */
+
+                    var requiredFaceAttributes = new FaceAttributeType[] {
+                        FaceAttributeType.Age,
+                        FaceAttributeType.Gender,
+                        FaceAttributeType.Smile,
+                        FaceAttributeType.Emotion,
+                        FaceAttributeType.HeadPose,
+                        FaceAttributeType.Glasses
+                    };
+
+                    var faces = await faceServiceClient.DetectAsync(imageFileStream,
+                        returnFaceLandmarks: true,
+                        returnFaceAttributes: requiredFaceAttributes);
+
+                    foreach (var face in faces)
+                    {
+                        var id = face.FaceId;
+                        var attributes = face.FaceAttributes;
+                        var age = attributes.Age;
+                        var gender = attributes.Gender;
+                        var smile = attributes.Smile;
+                        var facialHair = attributes.FacialHair;
+                        var headPose = attributes.HeadPose;
+                        var glasses = attributes.Glasses;
+                    }
+
+                    var faceRects = faces.Select(face => face.FaceRectangle);
+                    return faceRects.ToArray();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error");
+                return new FaceRectangle[0];
+            }
+        }
+
+        private void FacePhoto_MouseMove(object sender, MouseEventArgs e)
+        {
+            //TODO
+        }
+    }
+}
