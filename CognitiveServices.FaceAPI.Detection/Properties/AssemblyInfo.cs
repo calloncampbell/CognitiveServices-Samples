cf0505e3ed54a783ca01ce3b45cfae023165be41
Callon Campbell <calloncampbell@msn.com>
2018-03-04 00:47:33 -0500

    Refactored code for Face Detection. Added initial commit for Face Verification.

--- CognitiveServices.FaceAPI.Detection/Properties/AssemblyInfo.cs
+++ CognitiveServices.FaceAPI.Detection/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("VerifyCrew")]
+[assembly: AssemblyTitle("CognitiveServices.FaceAPI.Detection")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("VerifyCrew")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyProduct("CognitiveServices.FaceAPI.Detection")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
--- CognitiveServices.FaceAPI.Verification/Properties/Settings.Designer.cs
+++ CognitiveServices.FaceAPI.Verification/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace VerifyCrew.Properties {
+namespace CognitiveServices.FaceAPI.Verification.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
