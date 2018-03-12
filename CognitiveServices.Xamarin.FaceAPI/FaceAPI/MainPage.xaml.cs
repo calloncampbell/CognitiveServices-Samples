05c5f8c00b735cbf2e07435a608c455b985b8eef
Callon Campbell <calloncampbell@msn.com>
2018-03-11 23:36:50 -0400

    Cleanup

--- CognitiveServices.Xamarin.FaceAPI/FaceAPI/MainPage.xaml.cs
+++ CognitiveServices.Xamarin.FaceAPI/FaceAPI/MainPage.xaml.cs
@@ -21,7 +21,73 @@ namespace FaceAPI
         {
             InitializeComponent();
 
-            this.faceServiceClient = new FaceServiceClient("496b22058ec24307bbd09b0654f5fa03");
+            this.faceServiceClient = new FaceServiceClient("YOUR-SUBSCRIPTION-KEY-GOES-HERE");
+        }
+
+        private async Task<FaceDetection> DetectFacesAsync(MediaFile inputFile)
+        {
+            if (!CrossConnectivity.Current.IsConnected)
+            {
+                await DisplayAlert("Network error", "Please check your network connection and retry.", "OK");
+                return null;
+            }
+
+            // The list of Face attributes to return.
+            IEnumerable<FaceAttributeType> faceAttributes = new FaceAttributeType[]
+            {
+                FaceAttributeType.Gender,
+                FaceAttributeType.Age,
+                FaceAttributeType.Smile,
+                FaceAttributeType.Emotion,
+                FaceAttributeType.Glasses,
+                FaceAttributeType.FacialHair,
+                FaceAttributeType.HeadPose,
+            };
+
+            try
+            {
+                // Call the Face API.
+                Face[] faces = await faceServiceClient.DetectAsync(
+                    inputFile.GetStream(),
+                    returnFaceId: true,
+                    returnFaceLandmarks: false,
+                    returnFaceAttributes: faceAttributes);
+
+                if (faces.Length == 0)
+                {
+                    return null;
+                }
+
+                // Get highest rated emotion
+                var emotion = faces[0].FaceAttributes.Emotion.ToRankedList();
+
+                FaceDetection theData = new FaceDetection()
+                {
+                    Age = faces[0].FaceAttributes.Age,
+                    Beard = faces[0].FaceAttributes.FacialHair.Beard,
+                    Emotion = emotion.FirstOrDefault().Key,
+                    Gender = faces[0].FaceAttributes.Gender,
+                    Glasses = faces[0].FaceAttributes.Glasses.ToString(),
+                    Moustache = faces[0].FaceAttributes.FacialHair.Moustache,
+                    Smile = faces[0].FaceAttributes.Smile
+                };
+
+                this.BindingContext = theData;
+                this.Indicator1.IsRunning = false;
+                this.Indicator1.IsVisible = false;
+
+                return theData;
+            }
+            catch (FaceAPIException f)
+            {
+                await DisplayAlert("Network error", f.ErrorMessage, "OK");
+                return null;
+            }
+            catch (Exception e)
+            {
+                await DisplayAlert("Error", e.Message, "OK");
+                return null;
+            }
         }
 
         private async void UploadPictureButton_Clicked(object sender, EventArgs e)
@@ -42,6 +108,7 @@ namespace FaceAPI
             this.Indicator1.IsRunning = true;
             Image1.Source = ImageSource.FromStream(() => file.GetStream());
 
+            // Call Face API
             await DetectFacesAsync(file);
 
             this.Indicator1.IsRunning = false;
@@ -73,75 +140,11 @@ namespace FaceAPI
             this.Indicator1.IsRunning = true;
             Image1.Source = ImageSource.FromStream(() => file.GetStream());
 
+            // Call Face API
             await DetectFacesAsync(file);
 
             this.Indicator1.IsRunning = false;
             this.Indicator1.IsVisible = false;
-        }
-
-        private async Task<FaceDetection> DetectFacesAsync(MediaFile inputFile)
-        {
-            if (!CrossConnectivity.Current.IsConnected)
-            {
-                await DisplayAlert("Network error", "Please check your network connection and retry.", "OK");
-                return null;
-            }
-
-            // The list of Face attributes to return.
-            IEnumerable<FaceAttributeType> faceAttributes = new FaceAttributeType[]
-            {
-                FaceAttributeType.Gender,
-                FaceAttributeType.Age,
-                FaceAttributeType.Smile,
-                FaceAttributeType.Emotion,
-                FaceAttributeType.Glasses,
-                FaceAttributeType.FacialHair,
-                FaceAttributeType.HeadPose,
-            };
-
-            // Call the Face API.
-            try
-            {
-
-                Face[] faces = await faceServiceClient.DetectAsync(
-                    inputFile.GetStream(),
-                    returnFaceId: true,
-                    returnFaceLandmarks: false,
-                    returnFaceAttributes: faceAttributes);
-
-                if (faces.Length == 0)
-                {
-                    return null;
-                }
-
-                var emotion = faces[0].FaceAttributes.Emotion.ToRankedList();
-                FaceDetection theData = new FaceDetection()
-                { 
-                    Age = faces[0].FaceAttributes.Age,
-                    Beard = faces[0].FaceAttributes.FacialHair.Beard,
-                    Emotion = emotion.FirstOrDefault().Key,
-                    Gender = faces[0].FaceAttributes.Gender,
-                    Glasses = faces[0].FaceAttributes.Glasses.ToString(),
-                    Moustache = faces[0].FaceAttributes.FacialHair.Moustache,
-                    Smile = faces[0].FaceAttributes.Smile
-                };
-
-                this.BindingContext = theData;
-                this.Indicator1.IsRunning = false;
-                this.Indicator1.IsVisible = false;
-
-                return theData;
-            }
-            catch (FaceAPIException f)
-            {
-                await DisplayAlert("Network error", f.ErrorMessage, "OK");
-                return null;
-            }
-            catch (Exception e)
-            {
-                await DisplayAlert("Error", e.Message, "OK");
-                return null;
-            }  
-        }
+        }        
     }
 }
